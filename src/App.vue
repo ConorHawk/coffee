581e27dc21d8ad819e19ddd7762555de4b3c0d45
ConorHawk <conornhawkins@gmail.com>
2018-07-03 09:39:40 +1200

    added pattern back

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app" class="h-full min-h-full font-sans text-grey-darkest bg-coffee-light leading-normal font-light">
+  <div id="app" class="h-full min-h-full font-sans text-grey-darkest bg-pattern leading-normal font-light">
       <router-view></router-view>
   </div>
 </template>
@@ -14,5 +14,9 @@ export default {
   @import 'src/assets/css/style.scss';
   html, body {
     height: 100%;
+  }
+  .bg-pattern {
+    background-color: #1a1a19;
+    background-image: url("data:image/svg+xml,%3Csvg width='60' height='60' viewBox='0 0 60 60' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='none' fill-rule='evenodd'%3E%3Cg fill='%233c352a' fill-opacity='0.4'%3E%3Cpath d='M36 34v-4h-2v4h-4v2h4v4h2v-4h4v-2h-4zm0-30V0h-2v4h-4v2h4v4h2V6h4V4h-4zM6 34v-4H4v4H0v2h4v4h2v-4h4v-2H6zM6 4V0H4v4H0v2h4v4h2V6h4V4H6z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E");
   }
 </style>
